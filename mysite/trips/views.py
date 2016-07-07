bc2009245ce79a0fcea4979669abc4ba1c3cc2b1
yale918 <yale918@gmail.com>
2016-07-08 03:25:52 +0800

    first django app

---
+++ mysite/trips/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from datetime import datetime
+#from django.http import HttpResponse
+
+#from django.shortcuts import render
+
+
+def hello_world(request):
+    return render(request, 'hello_world.html', {
+        'current_time': datetime.now(),
+    })
+
+
+from .models import Post
+
+def home(request):
+	post_list = Post.objects.all()
+	return render(request, 'home.html',{
+		'post_list' : post_list,
+	})
+
+def post_detail(request, pk):
+	post = Post.objects.get(pk=pk)
+	return render(request, 'post.html',{'post': post})
